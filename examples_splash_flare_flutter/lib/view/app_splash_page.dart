1738e163e8d00dbef635a8494204fa836f5e8abb
KAMIYA.Atsuteru <a-kamiya@pscsrv.co.jp>
2020-08-13 15:51:40 +0900

    Feature: Implemented roulette animation.

--- examples_splash_flare_flutter/lib/view/app_splash_page.dart
+++ examples_splash_flare_flutter/lib/view/app_splash_page.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:flare_flutter/flare.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_controls.dart';
 import 'package:flutter/material.dart';
 
 class AppSplashPage extends StatefulWidget {
@@ -10,6 +14,7 @@ class _AppSplashPageState extends State<AppSplashPage>{
 
   @override
   Widget build(BuildContext context) {
+    var resultNumber = Random().nextInt(6) + 1;
     return WillPopScope(
         onWillPop: () async => false,
         child: Container(
@@ -17,15 +22,44 @@ class _AppSplashPageState extends State<AppSplashPage>{
           height: double.infinity,
           color: Colors.blueAccent,
           child: FlareActor(
-          'assets/app_splash.flr',
+          'assets/app_splash_roulette.flr',
           alignment: Alignment.center,
           fit: BoxFit.contain,
-          animation: 'all',
-          callback: (name) {
-            Navigator.of(context).pushReplacementNamed("/home");
-          },
+          controller: _AppSplashController(
+            number: resultNumber,
+            callback: (name) {
+              Future.delayed(Duration(milliseconds: 500)).then((value) {
+                Navigator.of(context).pushReplacementNamed("/home");
+              });
+            },
+          ),
         )
       )
     );
+  }
+}
+
+class _AppSplashController extends FlareControls {
+
+  final int number;
+  final FlareCompletedCallback callback;
+  _AppSplashController({
+    @required this.number,
+    this.callback
+  });
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    super.initialize(artboard);
+    play('shuffle');
+  }
+
+  @override
+  void onCompleted(String name) {
+    if (name == 'shuffle') {
+      play('result_$number');
+      return;
+    }
+    callback?.call(name);
   }
 }
